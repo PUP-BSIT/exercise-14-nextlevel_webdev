aea4bccc1a73b3fec749b77b7faa768c2c4c520f
alima-kailamarie <12malvar.alimakailamarie@gmail.com>
2023-11-13 22:27:18 +0800

    edit the comment button

--- alima_kailamarie/comment.js
+++ alima_kailamarie/comment.js
@@ -1,36 +1,51 @@
 document.addEventListener("DOMContentLoaded", function () {
-// Enable the "Comment" button when both fields are filled out
-const nameInput = document.getElementById("name");
-const commentInput = document.getElementById("comment");
-const commentButton = document.getElementById("comment_button");
-const commentDisplay = document.getElementById("comment_Display");
+    // Enable the "Comment" button when both fields are filled out
+    const nameInput = document.getElementById("name");
+    const commentInput = document.getElementById("comment");
+    const commentButton = document.getElementById("comment_button");
+    const commentDisplay = document.getElementById("comment_display");
 
-nameInput.addEventListener("input", toggleCommentButton);
-commentInput.addEventListener("input", function () {
-    toggleCommentButton();
+    nameInput.addEventListener("input", toggleCommentButton);
+    commentInput.addEventListener("input", function () {
+        toggleCommentButton();
         commentDisplay.textContent = commentInput.value;
-});
+    });
 
-function toggleCommentButton() {
-    commentButton.disabled = !(nameInput.value && commentInput.value);
-}
+    function toggleCommentButton() {
+        commentButton.disabled = !(nameInput.value && commentInput.value);
+    }
 
-document.getElementById('comment_form').addEventListener('submit', 
-function(event) {
-event.preventDefault(); 
+    document.getElementById('comment_form').addEventListener('submit', 
+    function(event) {
+        event.preventDefault(); 
 
-const name = nameInput.value;
-const comment = commentInput.value;
+        const name = nameInput.value;
+        const comment = commentInput.value;
+        const timestamp = new Date().getTime();
 
-const newComment = document.createElement('div');
-newComment.classList.add('comment');
-newComment.innerHTML = `<strong>${name}:</strong> ${comment}`;
+        const newComment = document.createElement('div');
+        newComment.classList.add('comment');
+        newComment.setAttribute('data-timestamp', timestamp);
+        newComment.innerHTML = `<strong>${name}:</strong> ${comment}`;
 
-commentDisplay.insertBefore(newComment, commentDisplay.firstChild);
+        commentDisplay.insertBefore(newComment, commentDisplay.firstChild);
 
-nameInput.value = '';
-commentInput.value = '';
+        nameInput.value = '';
+        commentInput.value = '';
 
-commentButton.disabled = true;
-});
+        commentButton.disabled = true;
+
+        sortComments();
+    });
+
+    function sortComments() {
+        const comments = commentDisplay.children;
+        Array.from(comments).sort(function(a, b) {
+            const timestampA = parseInt(a.getAttribute('data-timestamp'));
+            const timestampB = parseInt(b.getAttribute('data-timestamp'));
+            return timestampB - timestampA;
+        }).forEach(function(comment) {
+            commentDisplay.appendChild(comment);
+        });
+    }
 });
